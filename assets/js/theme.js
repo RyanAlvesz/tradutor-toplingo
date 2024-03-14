ecaa5a614fe790766380e6c17aca0d72846c2f8a
Ryan Alves <ra546038@gmail.com>
2024-03-14 12:23:09 -0300

    front-end

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -4,9 +4,11 @@ const darkThemeButton = document.getElementById('dark-mode')
 
 const addTheme = () => {
     if (localStorage.theme == 'dark'){
-        document.documentElement.classList.add('dark')
+        document.documentElement.classList.add('dark')        
+        const changeLanguageButton = document.getElementById('change-translate').children[0].src = './assets/img/change-white.svg'
     } else {
         document.documentElement.classList.remove('dark')
+        const changeLanguageButton = document.getElementById('change-translate').children[0].src = './assets/img/change.svg'
     }
 }    
 
